126727f1ac15941db088a7ffee1c0b65512f8fa9
KevinFire2030 <109524169+KevinFire2030@users.noreply.github.com>
2023-06-20 06:43:49 +0900

    19.1.2 (개발 5일차) 일별 데이터 가져오기

--- book/ch19/pymon.py
+++ book/ch19/pymon.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
 import Kiwoom
+import time
+from pandas import DataFrame
+import datetime
 
 MARKET_KOSPI   = 0
 MARKET_KOSDAQ  = 10
@@ -15,9 +18,23 @@ class PyMon:
         self.kospi_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSPI)
         self.kosdaq_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSDAQ)
 
+    def get_ohlcv(self, code, start):
+        self.kiwoom.ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}
+
+        self.kiwoom.set_input_value("종목코드", code)
+        self.kiwoom.set_input_value("기준일자", start)
+        self.kiwoom.set_input_value("수정주가구분", 1)
+        self.kiwoom.comm_rq_data("opt10081_req", "opt10081", 0, "0101")
+        time.sleep(0.2)
+
+        df = DataFrame(self.kiwoom.ohlcv, columns=['open', 'high', 'low', 'close', 'volume'],
+                       index=self.kiwoom.ohlcv['date'])
+        return df
+
+
     def run(self):
-        print(self.kospi_codes[0:4])
-        print(self.kosdaq_codes[0:4])
+        df = self.get_ohlcv("039490", "20230619")
+        print(df)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
