8962f1b96ca796b821acdf3a32d9a7886088aa6b
KevinFire2030 <109524169+KevinFire2030@users.noreply.github.com>
2023-06-20 06:13:47 +0900

    18.4.3 자동 주문 구현하기

--- book/ch18/pytrader.py
+++ book/ch18/pytrader.py
@@ -12,6 +12,8 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
 
+        self.trade_stocks_done = False
+
         self.kiwoom = Kiwoom()
         self.kiwoom.comm_connect()
 
@@ -26,7 +28,7 @@ class MyWindow(QMainWindow, form_class):
         accouns_num = int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
         accounts = self.kiwoom.get_login_info("ACCNO")
 
-        accounts_list = accounts.split(';')[0:accouns_num]
+        accounts_list = accounts.split(';')[1:accouns_num]
         self.comboBox.addItems(accounts_list)
 
         self.lineEdit.textChanged.connect(self.code_changed)
@@ -34,6 +36,62 @@ class MyWindow(QMainWindow, form_class):
         self.pushButton_2.clicked.connect(self.check_balance)
 
         self.load_buy_sell_list()
+
+    def trade_stocks(self):
+        hoga_lookup = {'지정가': "00", '시장가': "03"}
+
+        f = open("buy_list.txt", 'rt', encoding='UTF8')
+        buy_list = f.readlines()
+        f.close()
+
+        f = open("sell_list.txt", 'rt', encoding='UTF8')
+        sell_list = f.readlines()
+        f.close()
+
+        # account
+        account = self.comboBox.currentText()
+
+        # buy list
+        for row_data in buy_list:
+            split_row_data = row_data.split(';')
+            hoga = split_row_data[2]
+            code = split_row_data[1]
+            num = split_row_data[3]
+            price = split_row_data[4]
+
+            if split_row_data[-1].rstrip() == '매수전':
+                self.kiwoom.send_order("send_order_req", "0101", account, 1, code, num, price, hoga_lookup[hoga], "")
+
+        # sell list
+        for row_data in sell_list:
+            split_row_data = row_data.split(';')
+            hoga = split_row_data[2]
+            code = split_row_data[1]
+            num = split_row_data[3]
+            price = split_row_data[4]
+
+            if split_row_data[-1].rstrip() == '매도전':
+                self.kiwoom.send_order("send_order_req", "0101", account, 2, code, num, price, hoga_lookup[hoga], "")
+
+        # buy list
+        for i, row_data in enumerate(buy_list):
+            buy_list[i] = buy_list[i].replace("매수전", "주문완료")
+
+        # file update
+        f = open("buy_list.txt", 'wt', encoding='UTF8')
+        for row_data in buy_list:
+            f.write(row_data)
+        f.close()
+
+        # sell list
+        for i, row_data in enumerate(sell_list):
+            sell_list[i] = sell_list[i].replace("매도전", "주문완료")
+
+        # file update
+        f = open("sell_list.txt", 'wt', encoding='UTF8')
+        for row_data in sell_list:
+            f.write(row_data)
+        f.close()
 
     def load_buy_sell_list(self):
         f = open("buy_list.txt", 'rt', encoding='UTF8')
@@ -92,7 +150,13 @@ class MyWindow(QMainWindow, form_class):
                                hoga_lookup[hoga], "")
 
     def timeout(self):
+        market_start_time = QTime(9, 0, 0)
         current_time = QTime.currentTime()
+
+        if current_time > market_start_time and self.trade_stocks_done is False:
+            self.trade_stocks()
+            self.trade_stocks_done = True
+
         text_time = current_time.toString("hh:mm:ss")
         time_msg = "현재시간: " + text_time
 
