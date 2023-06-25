552255271af155cde88cde6b70b2c44583c0be0b
KevinFire2030 <109524169+KevinFire2030@users.noreply.github.com>
2023-06-25 11:58:53 +0900

    10.4 수정주가 크롤링

---
+++ quant_py/ch10/10.3.py
@@ -0,0 +1,36 @@
+import time
+import json
+import requests as rq
+import pandas as pd
+from tqdm import tqdm
+
+biz_day = '20230623'
+
+sector_code = [
+    'G25', 'G35', 'G50', 'G40', 'G10', 'G20', 'G55', 'G30', 'G15', 'G45'
+]
+
+data_sector = []
+
+for i in tqdm(sector_code):
+    url = f'''http://www.wiseindex.com/Index/GetIndexComponets?ceil_yn=0&dt={biz_day}&sec_cd={i}'''    
+    data = rq.get(url).json()
+    data_pd = pd.json_normalize(data['list'])
+
+    data_sector.append(data_pd)
+
+    time.sleep(2)
+
+kor_sector = pd.concat(data_sector, axis = 0)
+kor_sector = kor_sector[['IDX_CD', 'CMP_CD', 'CMP_KOR', 'SEC_NM_KOR']]
+kor_sector['기준일'] = biz_day
+kor_sector['기준일'] = pd.to_datetime(kor_sector['기준일'])
+
+# DB에 저장하기
+import sqlite3
+
+con = sqlite3.connect("kor_sector.db")
+kor_sector.to_sql('kor_sector', con, if_exists='replace')
+
+
+print('ok')
